743246afcca1041b5165a404835166c536d2d08d
Reilly Grant <reillyg@google.com>
2022-11-09 11:12:47 -0800

    Configure Web Bundle generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import eslint from 'vite-plugin-eslint'
+import webbundle from 'rollup-plugin-webbundle'
 
 export default {
-  plugins: [eslint()]
+  plugins: [
+    eslint(),
+    webbundle({
+      baseURL: '/',
+      static: { dir: 'public' },
+    })
+  ]
 }
