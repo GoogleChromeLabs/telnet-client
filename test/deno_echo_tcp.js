d75272fb2178ecb281394aadd56a46eaf53559b7
guest271314 <guest271314@gmail.com>
2023-08-13 09:27:17 -0700

    Create deno_echo_tcp.js
    Deno TCP server for local testing

---
+++ test/deno_echo_tcp.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env -S deno run -A
+// https://deno.land/manual@v1.36.1/examples/tcp_echo
+const encoder = new TextEncoder();
+const listener = Deno.listen({
+  port: 8000
+});
+const {hostname, port, transport} = listener.addr;
+console.log(`Listening on hostname: ${hostname}, port: ${port}, transport: ${transport}`);
+const abortable = new AbortController();
+const {
+  signal
+} = abortable;
+for await (const conn of listener) {
+  try {
+    const {
+      readable,
+      writable
+    } = conn;
+    const writer = writable.getWriter();
+    await readable.pipeThrough(new TextDecoderStream(), {
+      signal
+    }).pipeTo(new WritableStream({
+      async write(value, controller) {
+        console.log(value);
+        await writer.write(encoder.encode(value));
+      },
+      close() {
+        console.log('Stream closed');
+      },
+      abort(reason) {
+        console.log({
+          reason
+        });
+      }
+    }), {
+      signal
+    }).then(() => {
+      throw new Error('Stream aborted')
+    }) 
+  } catch (e) {
+    console.log({
+      e
+    });
+    break;
+  }
+}
+try {
+  listener.unref();
+} catch (err) {
+  console.log({
+    err
+  });
+}
