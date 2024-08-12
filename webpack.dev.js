41a4af4dbe6742a393c776f02eaf4132b75de41d
Sam Richard <samrichard@google.com>
2024-08-12 17:01:19 -0400

    Configure dev server for HMR

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,13 +14,24 @@
  * limitations under the License.
  */
 
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
-    publicPath: ''
+    publicPath: '',
+  },
+  devServer: {
+    port: 4321,
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        protocol: 'ws',
+        hostname: 'localhost',
+        port: 4321,
+      },
+    },
   },
 });
